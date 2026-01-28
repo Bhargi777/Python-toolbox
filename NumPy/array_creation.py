258379f5ae20c1a7e0d983f3c52708149b92fdad
Bhargi777 <bhargavasrisai7@gmail.com>
2026-01-28 08:54:34 +0530

    First commit of NumPy

---
+++ NumPy/array_creation.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+a = np.array([1, 2, 3])
+b = np.array([[1, 2], [3, 4]])
+
+zeros = np.zeros((3, 3))
+ones = np.ones((2, 4))
+identity = np.eye(4)
+random_vals = np.random.rand(3, 3)
+
+print("Array a:", a)
+print("Array b:\n", b)
+print("Zeros:\n", zeros)
+print("Ones:\n", ones)
+print("Identity Matrix:\n", identity)
+print("Random Values:\n", random_vals)
+
+

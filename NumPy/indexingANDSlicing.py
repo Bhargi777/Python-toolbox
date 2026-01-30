415ae5e84443b19f29eddf58b2b498e66c0f355a
Bhargi777 <bhargavasrisai7@gmail.com>
2026-01-30 10:19:25 +0530

    Add : Mathematical and indexing and slicing

---
+++ NumPy/indexingANDSlicing.py
@@ -0,0 +1,11 @@
+import numpy as np
+arr = np.array([10, 20, 30, 40, 50])
+
+print(arr[0])      # first element
+print(arr[-1])     # last element
+print(arr[1:4])    # slicing
+
+# 2D slicing
+matrix = np.array([[1, 2, 3], [4, 5, 6]])
+print(matrix[0, 1])   # row 0, column 1
+
---
+++ NumPy/mathematical.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+print(a + b)
+print(a * b)
+print(a ** 2)
+
+# Universal functions
+print(np.sqrt(a))
+print(np.exp(a))
+print(np.log(a))
